d87f7a9d1ed8da36c0ecb819750ecf7d4ddbc9d4
Luis Juan Sanchez <luisjuansp@gmail.com>
2016-05-01 01:09:57 -0500

    YUML functional

--- AntlrProject/src/MadBasic/Main.java
+++ AntlrProject/src/MadBasic/Main.java
@@ -2,7 +2,10 @@ package MadBasic;
 
 
 import MadBasic.IDE.MainIDE;
+import MadBasic.Semantic.BasicSemantic;
 import MadBasic.VMachine.Machine;
+import MadBasic.YUML.YUMLParser;
+import org.antlr.v4.runtime.atn.BasicState;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
@@ -10,10 +13,10 @@ import java.io.IOException;
 /**
  * Created by lsanchez on 3/9/16.
  */
-public class Main  {
+public class Main {
 
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
 //       /*
         ParseTree tree = null;
@@ -28,17 +31,23 @@ public class Main  {
         TestingGround tester = new TestingGround();
 
         TestingGround.TestNum[] selectedTests = {
-//                TestingGround.TestNum.VARIABLE_TABLE,
-//                TestingGround.TestNum.SCOPETABLE,
-//                TestingGround.TestNum.PROC_TABLE,
-//                TestingGround.TestNum.OPERANDSTACK,
-//                TestingGround.TestNum.QUADRUPLELIST,
-//                TestingGround.TestNum.ERAS,
+                TestingGround.TestNum.VARIABLE_TABLE,
+                TestingGround.TestNum.SCOPETABLE,
+                TestingGround.TestNum.PROC_TABLE,
+                TestingGround.TestNum.OPERANDSTACK,
+                TestingGround.TestNum.QUADRUPLELIST,
+                TestingGround.TestNum.ERAS,
                 TestingGround.TestNum.CLASSES,
         };
 
         tester.testManager(selectedTests);
-       /* */
+
+        YUMLParser yuml = new YUMLParser();
+        yuml.makeDiagram(BasicSemantic.getInstance().getClassHashMap());
+        yuml.createDiagram();
+        yuml.runDiagram();
+
+        /**/
 
         /*
         MainIDE ide = MainIDE.getInstance();
